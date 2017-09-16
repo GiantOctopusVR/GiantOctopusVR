887cd0bfb02ee11ddcbeded7dc2237e65b99d13d
Somdat <tsomda@gmail.com>
2017-09-16 16:11:38 -0700

    adding media files

---
+++ Assets/Components/EventManager/NetworkEventManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class NetworkEventManager : MonoBehaviour {
+
+	[Command]
+	public void CmdTriggerEvent(string eventName)
+	{
+		RpcTriggerEvent(eventName);
+	}
+
+	[ClientRpc]
+	public void RpcTriggerEvent(string eventName)
+	{
+		EventManager.TriggerEvent(eventName);
+	}
+}
