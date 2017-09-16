7e418e75179a76db3504c38de0df0a8061a14160
Avery Wagar <ajmw.subs@gmail.com>
2017-09-16 11:40:45 -0700

    added menu

---
+++ Assets/Components/MenuManager/MenuManagaer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagaer : MonoBehaviour {
+  public GameObject settingsMenu;
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+  public void loadScence(){
+    SceneManager.LoadScene(1);
+    Debug.Log("Loading Scence 1");
+  }
+  public void buttonQuit(){
+    Application.Quit();
+    Debug.Log("Qutiting Application");
+  }
+  public void openSettings(){
+    if(settingsMenu.active){
+      settingsMenu.SetActive(false);
+      Debug.Log("Enabling");
+    }
+    else {
+      settingsMenu.SetActive(true);
+      Debug.Log("Disabling");
+    }
+  }
+}
