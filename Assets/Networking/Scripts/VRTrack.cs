1aff85363e950ef8b73738320f60c70442785b78
teelrc@gmail.com <teelrc@gmail.com>
2017-09-16 01:23:16 -0700

    Adding networking test

---
+++ Assets/Networking/Scripts/VRTrack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class VRTrack : NetworkBehaviour {
+
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		transform.position = obj.transform.position;
+	}
+}
