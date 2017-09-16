96bbe8a9302b142c3ad33a9e8faed4df4c188c27
Xandon Frogget <xandon@hotmail.com>
2017-09-16 13:24:26 -0800

    Enemy AI
    Nav mesh and Enemy Spawner on grid

---
+++ Assets/Xandon/XandonScripts/PoolSpawner/PoolManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PoolManager : MonoBehaviour {
+
+	Dictionary<int,Queue<ObjectInstance>> poolDictionary = new Dictionary<int, Queue<ObjectInstance>> ();
+
+	static PoolManager _instance;
+
+    private Transform nexus;
+
+    void Start()
+    {
+        nexus = GameObject.FindGameObjectWithTag("Nexus").transform;
+    }
+
+
+    public static PoolManager instance {
+		get{ 
+			if (_instance == null) {
+				_instance = FindObjectOfType<PoolManager> ();
+			}
+			return _instance;
+		}
+	}
+
+	public void CreatePool(GameObject prefab, int poolSize){
+		int poolKey = prefab.GetInstanceID ();
+
+		GameObject poolHolder = new GameObject (prefab.name + " pool");
+		poolHolder.transform.parent = transform;
+
+		if(!poolDictionary.ContainsKey (poolKey)) {
+			poolDictionary.Add (poolKey, new Queue<ObjectInstance> ());
+
+			for(int i = 0; i< poolSize; i++){
+				ObjectInstance newObject = new ObjectInstance (Instantiate (prefab) as GameObject);
+				poolDictionary [poolKey].Enqueue (newObject);
+				newObject.setParent (poolHolder.transform);
+			}
+		}
+	}
+
+	public void ReuseObject(GameObject prefab, Vector3 position, Quaternion rotation, float scale) {
+        Debug.Log(position);
+		int poolKey = prefab.GetInstanceID ();
+
+		if (poolDictionary.ContainsKey (poolKey)) {
+			ObjectInstance ObjectToReuse = poolDictionary [poolKey].Dequeue ();
+			poolDictionary [poolKey].Enqueue (ObjectToReuse);
+
+			ObjectToReuse.Reuse (position, rotation, scale, nexus);
+        }
+    }
+
+	public class ObjectInstance {
+
+		public GameObject gameObject;
+		Transform transform;
+
+		bool hasPoolObjectComponent;
+		PoolObject poolObjectScript;
+
+		public ObjectInstance(GameObject objectInstance){
+			gameObject = objectInstance;
+			transform = gameObject.transform;
+			gameObject.SetActive(false);
+            var agent = gameObject.GetComponent<NavMeshAgent>();
+            agent.enabled = false;
+
+            if (gameObject.GetComponent<PoolObject>()) {
+				hasPoolObjectComponent = true;
+				poolObjectScript = gameObject.GetComponent<PoolObject>();
+			}
+		}
+
+		public void Reuse(Vector3 position, Quaternion rotation, float scale, Transform nexus) {
+
+			if (hasPoolObjectComponent) {
+				poolObjectScript.OnObjectReuse ();
+			}
+
+			gameObject.SetActive (true);
+			transform.position = position;
+			transform.rotation = rotation;
+            transform.localScale = new Vector3(scale, scale, scale);
+            var agent = gameObject.GetComponent<NavMeshAgent>();
+            agent.enabled = true;
+            agent.SetDestination(nexus.position);
+        }
+
+		public void setParent(Transform parent){
+			transform.parent = parent;
+		}
+	}
+}
