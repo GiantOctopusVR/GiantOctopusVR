078647622587f1d295c916698ddb1af0ca94b975
Somdat <tsomda@gmail.com>
2017-09-16 22:36:32 -0700

    added octo colliders

---
+++ Assets/OctoDefender/Scripts/TentacleWaker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleWaker : MonoBehaviour {
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		animator.SetBool("isRaised", true);
+		StartCoroutine(DoSomething());
+	}
+
+	void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag.Equals("Player")) {
+			print("trigger player");
+			animator.SetBool("isRaised", true);
+		}
+    }
+
+	void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag.Equals("Player")) {
+			print("trigger player");
+			animator.SetBool("isRaised", false);
+		}
+    }
+
+	IEnumerator DoSomething()
+  {
+       yield return new WaitForSeconds(1);
+	animator.SetBool("isRaised", false);
+	   
+  }
+}
