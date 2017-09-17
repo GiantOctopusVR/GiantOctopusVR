c087bf4e6fc2b47010ebdd9ed873bfe28d62a657
Brint E. Kriebel <github@bekit.net>
2017-09-17 16:07:41 -0700

    ARCore: Demo version
    Working fairly well. Tracking the octo, enemy disabled.

--- Assets/OctoDefender/Scripts/HeadFollow.cs
+++ Assets/OctoDefender/Scripts/HeadFollow.cs
@@ -22,9 +22,10 @@ public class HeadFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (head) {
-			float h_scale = head.transform.position.y / h;
+            //float h_scale = head.transform.position.y / h;
+            float h_scale = 1;
 
-			transform.position = new Vector3 (head.transform.position.x, head.transform.position.y - (headoffset*h_scale), head.transform.position.z);
+            transform.position = new Vector3 (head.transform.position.x, head.transform.position.y - (headoffset*h_scale), head.transform.position.z);
 			transform.position = transform.position - (head.transform.forward) * bodyoffset;
 
 			transform.localScale = new Vector3 (sx, sy * h_scale, sz);
