0e7758bc389d0ddb59c8ad0530f1a9a05d60b7d5
Bharat Ahluwalia <bharat@xzandy.com>
2017-09-16 17:41:43 -0700

    First Hololens push

---
+++ Assets/Scripts/TableBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+public class TableBehavior : MonoBehaviour, IInputClickHandler {
+    private GameObject cubePrefab;
+    private TextMesh UIText;
+    private void Start()
+    {
+        cubePrefab = GameObject.FindGameObjectWithTag("Tower");
+        UIText = GameObject.FindGameObjectWithTag("UIText").GetComponent<TextMesh>();
+    }
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Trying to instantiate cube on table plane");
+        UIText.text = "Trying to instantiate cube on table plane";
+        Instantiate(cubePrefab, GazeManager.Instance.HitPosition, Quaternion.identity);
+    }
+}
