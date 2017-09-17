ce7df19a03030a4df3a21169569c29e6ddc87d6a
Bharat Ahluwalia <bharat@xzandy.com>
2017-09-17 00:31:11 -0700

    cleaned up the scene

--- Assets/Components/Network/Scripts/BasicNetworkCharacter.cs
+++ Assets/Components/Network/Scripts/BasicNetworkCharacter.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour
+public class BasicNetworkCharacter : NetworkBehaviour
 {
-	void Update()
-	{
-		if (!isLocalPlayer)
-		{
-			return;
-		}
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
 
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
-	}
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
 }
+
---
+++ Assets/HoloNetClientStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HoloNetClientStart : MonoBehaviour {
+
+    NetworkManager netManager;
+	// Use this for initialization
+	void Start () {
+        netManager = gameObject.GetComponent<NetworkManager>();
+        netManager.StartClient();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Start Client Connection");
+            
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Start HOST Connection");
+            netManager.StartHost();
+        }
+    }
+}
