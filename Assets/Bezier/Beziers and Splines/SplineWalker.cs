6db33e150b962c04b0387505f6bc49643eef2342
chang47 <coralbue@gmail.com>
2017-09-17 04:14:57 -0700

    added bezier curves

---
+++ Assets/Bezier/Beziers and Splines/SplineWalker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineWalker : MonoBehaviour {
+
+	public BezierSpline spline;
+	public bool lookForward;
+	public float duration;
+	public float progress;
+	public SplineWalkerMode mode;
+	private bool goingForward = false; // true in origianl tutorial
+
+
+
+
+	private void Update() {
+		
+		if (goingForward == true) {
+			progress += Time.deltaTime / duration;
+			if (progress > 1f) {
+				if (mode == SplineWalkerMode.Once) {
+					progress = 1f;
+				} else if (mode == SplineWalkerMode.Loop) {
+					progress -= 1f;
+				} else {
+					progress = 2f - progress;
+					goingForward = false;
+				}
+			}
+		} else {
+			progress -= Time.deltaTime / duration;
+			if (progress < 0f) {
+				progress = -progress;
+				goingForward = true;
+			}
+
+		}
+		hammerPosition ();
+
+	}
+		
+	private void hammerPosition(){
+		if (goingForward = true && spline != null) {
+			Vector3 position = spline.GetPoint (progress);
+			transform.localPosition = position;
+			if (lookForward) {
+				transform.LookAt (position + spline.GetDirection (progress));
+			}
+		}
+	}
+
+    public void OnDisable()
+    {
+		gameObject.GetComponent<Rigidbody> ().velocity = spline.GetVelocity (progress);
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+    }
+
+    public void OnEnable()
+    {
+		
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+
+}
---
+++ Assets/Bezier/Scripts/DroneHealthController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneHealthController : MonoBehaviour {
+    public float Health = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    public void getHit(float damage)
+    {
+        if (Health <= 0)
+        {
+            return;
+        }
+        Health -= damage;
+        if (Health <= 0)
+        {
+            GetComponent<SplineWalker>().enabled = false;
+        }
+    }
+}
