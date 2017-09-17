8253434d2048a33e1b5a8a81e2e2aa92666f235b
Xandon Frogget <xandon@hotmail.com>
2017-09-17 00:28:50 -0800

    Add Kill to Enemy

---
+++ Assets/EnemyHit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour {
+
+        private float breakForce = 100f;
+
+
+        private void Start()
+        {
+            GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var collisionForce = GetCollisionForce(collision);
+
+            if (collisionForce > 100f)
+            {
+                Destroy(gameObject, 0.05f);
+            }
+
+        }
+
+        private float GetCollisionForce(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                return 500f;
+            }
+
+            if (collision.gameObject.tag == "Bullet")
+            {
+                return 500f;
+            }
+
+            if (collision.gameObject.tag == "Laser")
+            {
+                return 500f;
+            }
+
+            if (collision.gameObject.tag == "Bat")
+            {
+                return 500f;
+            }
+
+            return 0f;
+        }
+
+
+        private void EnemyDestroy(float force)
+        {
+            Destroy(gameObject, .05f);
+        }
+}
+
